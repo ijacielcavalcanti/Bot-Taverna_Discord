3721cd283f17a7c7bccf0d39d0cd48788d57a442
Ijaciel Cavalcanti Targino de Oliveira <ijacielcavalcanti21@gmail.com>
2026-06-20 22:29:26 -0300

    correção sintaxe setColor

--- commands/admin/setupPorta.js
+++ commands/admin/setupPorta.js
@@ -41,7 +41,7 @@ module.exports = {
         const linhaAlertas = new ActionRowBuilder().addComponents(menuAlertas);
 
         const embed = new EmbedBuilder()
-            .setcolor(0xD4AF37)
+            .setColor(0xD4AF37)
             .setTitle('🚪 O Mural de Contratos')
             .setDescription('Selecione abaixo quais estilos de jogo você joga para encontrar outros viajantes com os mesmos interesses.\n\nEscolha também quais alertas você deseja receber dos Arautos da Taverna. Você pode alterar suas escolhas a qualquer momento marcando ou desmarcando as opções.')
             .setImage('https://cdn.discordapp.com/attachments/1511518891594219540/1518024518084333618/PortaBoasvindasmain.png?ex=6a386a00&is=6a371880&hm=45603c0211adbc6527129299e10d172053f61288ff40e6f941bcd0d3c6f3d2a0&.png'); // Substitua pela imagem oficial da porta
